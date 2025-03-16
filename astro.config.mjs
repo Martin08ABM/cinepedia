e66b72ddd12bb9627c38dbfa4aa54aa37efda686
Martin08ABM <martin08abm@gmail.com>
2025-03-16 18:38:02 +0100

    Arreglo de la etiqueta de Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import partytown from '@astrojs/partytown';
 
 import vercel from '@astrojs/vercel';
 
@@ -11,4 +12,5 @@ export default defineConfig({
     },
     output: 'server',
     adapter: vercel(),
+    integrations: [partytown()],
 });
