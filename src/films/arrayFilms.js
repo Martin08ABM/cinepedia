a6c1bfc7c23b8b5e6ad7a6ed9784c538950a0c22
Martin08ABM <martin08abm@gmail.com>
2025-03-19 13:08:31 +0100

    Tercer commit 19/03/2025

--- src/films/arrayFilms.js
+++ src/films/arrayFilms.js
@@ -12,151 +12,151 @@ export const peliculas = [
     {
       title: "Salvar al soldado Ryan",
       category: "Guerra",
-      image: "../../public/Films-Images/salvar-al-soldado-ryan.jpeg",
+      image: "/Films-Images/salvar-al-soldado-ryan.jpeg",
     },
     {
       title: "Dunkerque",
       category: "Guerra",
-      image: "../../public/Films-Images/dunkerque.jpg",
+      image: "/Films-Images/dunkerque.jpg",
     },
     {
       title: "El Padrino",
       category: "Crimen",
-      image: "../../public/Films-Images/el-padrino.jpg",
+      image: "/Films-Images/el-padrino.jpg",
     },
     {
       title: "El irlandés",
       category: "Crimen",
-      image: "../../public/Films-Images/el-irlandes.jpeg",
+      image: "/Films-Images/el-irlandes.jpeg",
     },
     {
       title: "Misión Imposible: Fallout",
       category: "Acción",
-      image: "../../public/Films-Images/mision-imposible_fallout.jpeg",
+      image: "/Films-Images/mision-imposible_fallout.jpeg",
     },
     {
       title: "Spider-Man: Lejos de casa",
       category: "Acción",
-      image: "../../public/Films-Images/spiderman-lejos-de-casa.jpeg",
+      image: "/Films-Images/spiderman-lejos-de-casa.jpeg",
     },
     {
       title: "Vengadores: Endgame",
       category: "Acción",
-      image: "../../public/Films-Images/vengadores-endgame.jpg"
+      image: "/Films-Images/vengadores-endgame.jpg"
     },
     {
       title: "John Wick",
       category: "Acción",
-      image: "../../public/Films-Images/john-wick.jpg",
+      image: "/Films-Images/john-wick.jpg",
     },
     {
       title: "Indiana Jones y el reino de la calavera de cristal",
       category: "Aventura",
-      image: "../../public/Films-Images/indiana-jones-calavera-de-cristal.jpg",
+      image: "/Films-Images/indiana-jones-calavera-de-cristal.jpg",
     },
     {
       title: "Jurassic World",
       category: "Aventura",
-      image: "../../public/Films-Images/jurassic-world.jpg",
+      image: "/Films-Images/jurassic-world.jpg",
     },
     {
       title: "Piratas del Caribe: La maldición de la Perla Negra",
       category: "Aventura",
-      image: "../../public/Films-Images/piratas-del-caribe-perla-negra.jpg",
+      image: "/Films-Images/piratas-del-caribe-perla-negra.jpg",
     },
     {
       title: "La vida de Pi",
       category: "Aventura",
-      image: "../../public/Films-Images/la-vida-de-pi.jpg",
+      image: "/Films-Images/la-vida-de-pi.jpg",
     },
     {
       title: "Toy Story 4",
       category: "Animación",
-      image: "../../public/Films-Images/toy-story-4.jpeg",
+      image: "/Films-Images/toy-story-4.jpeg",
     },
     {
       title: "Coco",
       category: "Animación",
-      image: "../../public/Films-Images/coco.jpeg",
+      image: "/Films-Images/coco.jpeg",
     },
     {
       title: "Spider-Man: Un nuevo universo",
       category: "Animación",
-      image: "../../public/Films-Images/spiderman-un-nuevo-universo.jpeg",
+      image: "/Films-Images/spiderman-un-nuevo-universo.jpeg",
     },
     {
       title: "Los Increíbles 2",
       category: "Animación",
-      image: "../../public/Films-Images/los-increibles-2.jpeg",
+      image: "/Films-Images/los-increibles-2.jpeg",
     },
     {
       title: "Guerra de papás",
       category: "Comedia",
-      image: "../../public/Films-Images/guerra-de-papas.jpeg",
+      image: "/Films-Images/guerra-de-papas.jpeg",
     },
     {
       title: "Bad",
       category: "Comedia",
-      image: "../../public/Films-Images/bad-boys-for-life.jpeg",
+      image: "/Films-Images/bad-boys-for-life.jpeg",
     },
     {
       title: "La ciudad perdida",
       category: "Comedia",
-      image: "../../public/Films-Images/la-ciudad-perdida.jpeg"
+      image: "/Films-Images/la-ciudad-perdida.jpeg"
     },
     {
       title: "Noche de juegos",
       category: "Comedia",
-      image: "../../public/Films-Images/noche-de-juegos.jpeg",
+      image: "/Films-Images/noche-de-juegos.jpeg",
     },
     {
       title: "El juicio de los 7 de Chicago",
       category: "Drama",
-      image: "../../public/Films-Images/el-juicio-de-los-7-de-chicago.jpeg"
+      image: "/Films-Images/el-juicio-de-los-7-de-chicago.jpeg"
     },
     {
       title: "Figuras ocultas",
       category: "Drama",
-      image: "../../public/Films-Images/figuras-ocultas.jpeg",
+      image: "/Films-Images/figuras-ocultas.jpeg",
     },
     {
       title: "El lobo de Wall Street",
       category: "Drama",
-      image: "../../public/Films-Images/el-lobo-de-wall-street.jpeg",
+      image: "/Films-Images/el-lobo-de-wall-street.jpeg",
     },
     {
       title: "La llegada",
       category: "Drama",
-      image: "../../public/Films-Images/la-llegada.jpeg",
+      image: "/Films-Images/la-llegada.jpeg",
     },
     {
       title: "Dune",
       category: "Ciencia ficción",
-      image: "../../public/Films-Images/dune.jpeg",
+      image: "/Films-Images/dune.jpeg",
     },
     {
       title: "Interestelar",
       category: "Ciencia ficción",
-      image: "../../public/Films-Images/interestelar.jpg",
+      image: "/Films-Images/interestelar.jpg",
     },
     {
       title: "Proyecto Adam",
       category: "Ciencia ficción",
-      image: "../../public/Films-Images/proyecto-adam.jpeg",
+      image: "/Films-Images/proyecto-adam.jpeg",
     },
     {
       title: "Tenet",
       category: "Ciencia ficción",
-      image: "../../public/Films-Images/tenet.jpeg",
+      image: "/Films-Images/tenet.jpeg",
     },
     {
       title: "Un lugar tranquilo",
       category: "Terror",
-      image: "../../public/Films-Images/un-lugar-tranquilo.jpeg",
+      image: "/Films-Images/un-lugar-tranquilo.jpeg",
     },
     {
       title: "El teléfono negro",
       category: "Terror",
-      image: "../../public/Films-Images/el-telefono-negro.jpeg"
+      image: "/Films-Images/el-telefono-negro.jpeg"
     },
   ];
