6a2a284217804f03bef26ddb75933448d017cc44
Martin08ABM <martin08abm@gmail.com>
2025-04-02 16:47:01 +0200

    Actualización dia 02/03/2025

--- src/assets/arrayFilms.js
+++ src/assets/arrayFilms.js
@@ -1,62 +1,74 @@
 export const peliculas = [
     {
-        title: "American Sniper",
-        category: "Guerra",
-        image: "/Films-Images/american-snipr.jpg",
+        "id": "american-sniper",
+        "title": "American Sniper",
+        "category": "Guerra",
+        "image": "/Films-Images/american-snipr.jpg"
     },
     {
-        title: "Hasta el Último Hombre",
-        category: "Guerra",
-        image: "/Films-Images/hasta-el-ultimo-hombre.jpg",
+        "id": "hasta-el-ultimo-hombre",
+        "title": "Hasta el Último Hombre",
+        "category": "Guerra",
+        "image": "/Films-Images/hasta-el-ultimo-hombre.jpg"
     },
     {
-        title: "Coco",
-        category: "Animación",
-        image: "/Films-Images/coco.jpg",
+        "id": "coco",
+        "title": "Coco",
+        "category": "Animación",
+        "image": "/Films-Images/coco.jpg"
     },
     {
-        title: "Dunkerque",
-        category: "Guerra",
-        image: "/Films-Images/dunkerque.jpg",
+        "id": "dunkerque",
+        "title": "Dunkerque",
+        "category": "Guerra",
+        "image": "/Films-Images/dunkerque.jpg"
     },
     {
-        title: "El lobo de Wall Street",
-        category: "Drama",
-        image: "/Films-Images/el-lobo-de-wall-street.jpg",
+        "id": "el-lobo-de-wall-street",
+        "title": "El lobo de Wall Street",
+        "category": "Drama",
+        "image": "/Films-Images/el-lobo-de-wall-street.jpg"
     },
     {
-        title: "Interestelar",
-        category: "Ciencia ficción",
-        image: "/Films-Images/interestelar.jpg",
+        "id": "interestelar",
+        "title": "Interestelar",
+        "category": "Ciencia ficción",
+        "image": "/Films-Images/interestelar.jpg"
     },
     {
-        title: "John Wick",
-        category: "Acción",
-        image: "/Films-Images/john-wick.jpg",
+        "id": "john-wick",
+        "title": "John Wick",
+        "category": "Acción",
+        "image": "/Films-Images/john-wick.jpg"
     },
     {
-        title: "La llegada",
-        category: "Drama",
-        image: "/Films-Images/la-llegada.jpg",
+        "id": "la-llegada",
+        "title": "La llegada",
+        "category": "Drama",
+        "image": "/Films-Images/la-llegada.jpg"
     },
     {
-        title: "Los Increíbles 2",
-        category: "Animación",
-        image: "/Films-Images/los-increibles-2.jpg",
+        "id": "los-increibles-2",
+        "title": "Los Increíbles 2",
+        "category": "Animación",
+        "image": "/Films-Images/los-increibles-2.jpg"
     },
     {
-        title: "Salvar al soldado Ryan",
-        category: "Guerra",
-        image: "/Films-Images/salvar-al-soldado-ryan.jpg",
+        "id": "salvar-al-soldado-ryan",
+        "title": "Salvar al soldado Ryan",
+        "category": "Guerra",
+        "image": "/Films-Images/salvar-al-soldado-ryan.jpg"
     },
     {
-        title: "Spider-Man: Lejos de casa",
-        category: "Acción",
-        image: "/Films-Images/spiderman-lejos-de-casa.jpg",
+        "id": "spiderman-lejos-de-casa",
+        "title": "Spider-Man: Lejos de casa",
+        "category": "Acción",
+        "image": "/Films-Images/spiderman-lejos-de-casa.jpg"
     },
     {
-        title: "Vengadores: Endgame",
-        category: "Acción",
-        image: "/Films-Images/vengadores-endgame.jpg"
-    },
+        "id": "vengadores-endgame",
+        "title": "Vengadores: Endgame",
+        "category": "Acción",
+        "image": "/Films-Images/vengadores-endgame.jpg"
+    }
 ]
