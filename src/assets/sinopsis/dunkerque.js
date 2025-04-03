7b9d097b0bcf359503827bd926082554f19ef55f
Martin08ABM <martin08abm@gmail.com>
2025-04-03 10:03:59 +0200

    Primer Update 03/04/2025

---
+++ src/assets/sinopsis/dunkerque.js
@@ -0,0 +1,9 @@
+export const dunkerque = [
+    {
+        sinopsis: "En el año 1940, durante la Segunda Guerra Mundial, cientos de miles de soldados británicos y aliados se encuentran atrapados en las playas de Dunkerque, rodeados por el implacable avance del ejército alemán. La película narra la angustiosa y heroica Operación Dinamo, un intento desesperado por evacuar a estas tropas atrapadas. A través de tres perspectivas entrelazadas: la lucha en la playa, la desesperación en el mar y la valentía en el aire, Dunkerque sumerge al espectador en una experiencia cinematográfica intensa y conmovedora, donde la supervivencia y el espíritu humano son puestos a prueba en medio del caos de la guerra.",
+        actor1: "Fionn Whitehead como Tommy, un joven soldado británico que lucha por sobrevivir en las playas de Dunkerque.",
+        actor2: "Tom Hardy como Farrier, un valiente piloto de la Real Fuerza Aérea Británica que arriesga su vida en el aire para proteger a los soldados en tierra.",
+        actor3: "Kenneth Branagh como el comandante Bolton, un oficial de la Marina Real que supervisa la evacuación desde el muelle de Dunkerque.",
+        actor4: "Cillian Murphy como Claude, un soldado canadiense que lucha por sobrevivir en las playas de Dunkerque.",
+    }
+]
